f1246a1c37ecc62b0cc10de1146f40bf8e5b02aa
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-21 11:47:34 +0300

    Добавили задачу 2 на возведение в степень

---
+++ Example002_Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+string? string1=Console.ReadLine();
+int number = 0;
+
+if (string1 !=null)
+{
+    number = int.Parse(string1);
+}
+//Решение математическим путем
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+//Решение с использованием библиотеки (готовые функции С#)
+int sqr2 = Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine($"Квадрат числа {number} равен {sqr2}");
