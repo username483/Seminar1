7e98e707997c2f785aa717426641e16e613d2606
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-21 14:40:02 +0300

    Решена задача на вывод всех целых чисел от -а до а

---
+++ Example005_Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int number=int.Parse(Console.ReadLine());
+int i=number;
+
+
+if (number<0)
+{
+    number=number*-1;
+}
+else if (number >0)
+{
+    i=i*-1;
+}
+else{
+    System.Console.WriteLine("0");
+}
+while(i<number)
+{
+    System.Console.WriteLine(i);
+    i++;
+}
+System.Console.WriteLine(number);
