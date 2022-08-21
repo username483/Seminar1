1fe92d839aadaf5add2ca3e0ef0c762154d6106d
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-21 14:13:47 +0300

    Решена задача Дни недели

---
+++ Example004_Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int number=int.Parse(Console.ReadLine());
+
+if (number<=7)
+{if (number==1)
+    {Console.WriteLine("Понедельник");}
+if (number==2)
+    {Console.WriteLine("Вторник");}
+if (number==3)
+    {Console.WriteLine("Среда");}
+if (number==4)
+    {Console.WriteLine("Четверг");}
+if (number==5)
+    {Console.WriteLine("Пятница");}
+if (number==6)
+    {Console.WriteLine("Суббота");}
+if (number==7)
+    {Console.WriteLine("Воскресенье");}}
+else
+{Console.WriteLine("нет такого дня недели");}
