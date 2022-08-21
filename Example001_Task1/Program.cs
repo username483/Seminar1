45fb119961dee7081cd95bb727aa38c2eec4e822
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-21 11:30:19 +0300

    задача на возведение в степень решена

---
+++ Example001_Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear(); //очищает консоль перед каждым новым запуском программы
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine()); //Console.Readline работает 
+//только с типом переменных string (строковым), 
+//а нам нужны целые числа, поэтому мы добавляем Parse
+
+//Решение математическим путем
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");// Console.WriteLine работает
+//только с типом переменных string (строковым), 
+//а нам нужно вывести значения {number} {sqr}, поэтому мы добавляем амперсант $
+
+//Решение с использованием библиотеки (готовые функции С#)
+int sqr2 = Convert.ToInt32(Math.Pow(number,2));// Convert.ToInt32 конвертирует строковое значение
+//введенное с клавиатуры в тип данных int (аналогично int.Parse)
+// Math.Pow это возведение числа (number) в степень 2
+Console.WriteLine($"Квадрат числа {number} равен {sqr2}");
+
