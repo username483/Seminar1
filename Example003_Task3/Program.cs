a1f552163839e566395818720bb67990c052b61f
Olga Korchagina <olga_korchagina_2016@mail.ru>
2022-08-21 14:06:32 +0300

    Решена задача на определение квадрата числа

---
+++ Example003_Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите первое число ");
+int number1=int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2=int.Parse(Console.ReadLine());
+
+if (Convert.ToInt32(Math.Pow(number1,2))==number2)
+    {Console.WriteLine($"Квадрат числа {number1} равен введенному вами числу {number2}");}
+else
+   {Console.WriteLine($"Квадрат числа {number1} не равен введенному вами числу {number2}");}
+
+
+
+
+
+
